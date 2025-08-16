1e904f61ad8f74c4e5c545f9c905e20c15d8ca39
mtoguiyeni <mtoguiyeni@gmail.com>
2025-08-16 10:47:59 +0000

    modification de transform dans la racine du project

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # app.py
 import streamlit as st
 import pandas as pd
-from transform import clean_data
+from . import transform
 
 df = pd.read_csv("data/data.csv")
-df = clean_data(df)
+df = transform(df)
 st.line_chart(df.set_index("date")["value"])
